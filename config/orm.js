b4fdeb862b809f32db51cc0d848463230e6e28fa
Ashley <anoelj86@yahoo.com>
2018-07-26 18:36:01 -0600

    working

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-var connection = require('./connection');
+var connection = require('./connection.js');
 
 // * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 
@@ -6,37 +6,82 @@ var connection = require('./connection');
 // * `insertOne()`
 // * `updateOne()`
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
 
 var orm = {
 
-    selectAll: function (whatToSelect, tableInput) {
-        var queryString = "SELECT ?? FROM ??";
-        connection.query(queryString, [whatToSelect, tableInput], function (err, result) {
-            if (err) throw err;
+    selectAll: function (tableInput, cb) {
+        var queryString = " SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) { throw err };
             console.log(result);
+            cb(result);
         });
     },
 
-    insertOne: function () {
-        var queryString = "INSERT INTO burgers SET ?? WHERE ?? = ?";
-        connection.query(queryString, [], function (err, res) {
-            if (err) throw err;
+    insertOne: function (tableInput, col, val, cb) {
+        var queryString = "INSERT INTO" + tableInput;
 
+        queryString += " (";
+        queryString += col.toString();
+        queryString += ") ";
+        queryString += val + "(";
+        queryString += printQuestionMarks(val.length);
+        queryString += ") ";
+
+        console.log(queryString);
+        connection.query(queryString, val, function (err, res) {
+            if (err) { throw err };
+            cb(res);
         })
     },
 
-    updateOne: function () {
-        var queryString = "UPDATE burgers SET ?? WHERE ??"
+    updateOne: function (tableInput, col, val, cb) {
+        var queryString = "UPDATE" + tableInput;
+
+        queryString += " SET ";
+        queryString += objToSql(col);
+        queryString += " WHERE ";
+        queryString += val;
+
+        console.log(queryString);
+
+        connection.query(queryString, function (err, res) {
+            if (err) { throw err };
+            cb(res);
+        })
     }
-
-
-
-
-
-
-
-
-
 
 };
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,36 +1,41 @@
 var express = require('express');
 var router = express.Router();
-var burger = require('../models/burger.js');
+var burger = require('../models/burger');
+var orm = require('../config/orm');
 
 // 4. Create the `router` for the app, and export the `router` at the end of your file.
 
 // start at the index file
+
 router.get('/', function (req, res) {
     res.redirect('/index');
 });
 
 
 // use the selectAll function in orm file using burger data
+
 router.get('/index', function (req, res) {
-    burger.selectAll(function (data) {
+    orm.selectAll("burgers", function (data) {
         var object = { burgers: data };
+
         console.log(object);
-        res.render('index', object);
+        res.render("index", object);
     });
 });
 
 // Create a new burger using orm function and the users inputted data
-router.post('/burger/create', function (req, res) {
-    burger.insertOne(req.body.burger_name, function () {
-        res.redirect('/index');
-    });
-});
+
+// router.post('/burger/create', function (req, res) {
+//     orm.insertOne(req.body.burger_name, function () {
+//         res.redirect('/index');
+//     });
+// });
 
 // delete/eat a burger and update the screen using orm and user selection
-router.post('/burger/eat/:id', function (req, res) {
-    burger.updateOne(req.params.id, function () {
-        res.redirect('/index');
-    });
-});
+// router.post('/burger/eat/:id', function (req, res) {
+//     orm.updateOne(req.params.id, function () {
+//         res.redirect('/index');
+//     });
+// });
 
 module.exports = router
